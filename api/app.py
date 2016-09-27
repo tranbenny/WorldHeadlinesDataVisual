48f4b4f6e680b5d7ffd9a034edbb82341c732a41
Benny Tran <tran.benny123@gmail.com>
2016-09-27 12:15:58 -0700

    set up route for sample api call

--- api/app.py
+++ api/app.py
@@ -6,40 +6,36 @@ import time
 from flask import Flask, request, jsonify, Response, render_template, url_for
 from flask.ext.cors import CORS
 
+from DatabaseAccess.service.HeadlineSummaryService import HeadlineSummaryService
+
 
 
 app = Flask(__name__)
 CORS(app)
 cross_origin(app)
 
+service = HeadlineSummaryService()
+
 # API ROUTES
 # -------------------------------------------------------------------------------------------
-# @app.route('/api/headlines', methods=['GET'])
-# def getHeadlines():
-#     date = request.args.get('date')
-#     return getHeadlineSummaryData(date)
-#
-#
-# @app.route('/api/headlines/today', method=['GET'])
-# def getTodayHeadlines():
-#     date = time.strftime('%Y-%m-%d')
-#     return getHeadlineSummaryData(date)
-#
+
+@app.route('/api/sample', methods=['GET'])
+def getSample():
+    return jsonify(service.getSampleData())
 
 
-# WEB APPLICATION ROUTES
-# -------------------------------------------------------------------------------------------
-url_for('static', filename='index.html')
+@app.route('/api/headlines', methods=['GET'])
+def getHeadlines():
+     date = request.args.get('date')
+     return jsonify(service.findByDate(date))
 
-@app.errorhandler(404)
-def notFound():
-    message = {
-        'status': 404,
-        'message' : 'Route not found'
-    }
-    response = jsonify(message)
-    response.status_code = 404
-    return response
+
+@app.route('/api/headlines/today', methods=['GET'])
+def getTodayHeadlines():
+     date = time.strftime('%Y-%m-%d')
+     return jsonify(service.findByDate(date))
+
+
 
 
 
